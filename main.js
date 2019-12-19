2e99ce6d02814eda41a0c3b9cc5039418badebbb
Joel Robert Justiawan <joelwindows7@gmail.com>
2019-12-19 23:51:25 +0700

    Give up
    Times up. 20 december on 11 AM must submit. time's not worth it while uploader this time will hang at big demand moment

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 //Download Jquery now! https://jquery.com
+//Download text-fill plugin https://plugins.jquery.com/textfill/
+// https://stackoverflow.com/questions/687998/auto-size-dynamic-text-to-fill-fixed-size-container
 const MenuEnums = {
     Home : "homepage.html",
     Registration : "register.html",
@@ -8,7 +10,9 @@ const MenuEnums = {
     Contact : "ContactForm.html",
     Cart: "viewCart.html",
     OrderPage: "OrderNow.html",
-    Test : "CenterKeyTest.html"
+    Test : "CenterKeyTest.html",
+    Flight: "flight.html",
+    Hotel: "hotel.html"
 }
 var MenuIsOnRightNow = MenuEnums.Home;
 var ScreenIsSmall = false;
@@ -34,8 +38,30 @@ function UpdateScreenSmallBool(){
         }
     }, 1000)
 }
+// https://www.w3schools.com/howto/howto_js_remove_class.asp
+let ArrayOfNavigates = document.getElementsByClassName("Navitation")
+function ClearAddableBackGroundButton(){
+    // ArrayOfNavigates.forEach(element => {
+    //     element.classList.remove("AddableBlueButtonBackColoring")
+    // });
+    for (let Arrayam = 0; Arrayam < ArrayOfNavigates.length; Arrayam++) {
+        const element = ArrayOfNavigates[Arrayam];
+        element.classList.remove("AddableBlueButtonBackColoring")
+    }    
+    
+}
+function AddAddableBackGroundButtonOn(whichNumberIndex){
+    ArrayOfNavigates[whichNumberIndex].classList.add("AddableBlueButtonBackColoring")
+}
 // Home, Catalogue, Login, Register, Cart, Wishlist, History
 let HaveLoggedIn = false;
+let LoggedInAs;
+let LoginData =[
+    {
+        email: "me@mail.com",
+        pass: "12jpg" //irl, Password should be hash-salted! but eh, this is just dummy login system
+    }
+]
 
 let TestimonyData = [
     {
@@ -45,9 +71,24 @@ let TestimonyData = [
     {
         name: 'Bauhurst',
         Tanggap: 'It was a hard time whenever me and my family to travel. We have to went into somehow shady group of tiket company that mostly scams. But thanks to Trips seek, I can now be relieved for their legitimaxy of tickets! thanks you trip seek!!'
+    },
+    {
+        name: 'LoremIpsumGuy123',
+        Tanggap: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias, nihil, ipsam optio rerum, quia nulla perferendis fugit corrupti fuga cum eveniet magnam odio facere labore! Rerum ut quaerat est corporis.'
     }
 ]
+let TestimonyNumbers = TestimonyData.length;
+// let TestimonyNumberSelect = 0;
 let TestimonyBody = document.getElementById('TestimonyFieldMini');
+
+function CycleUpTetimonyIndex(){
+    if(TestimonyIndex > TestimonyNumbers-2){
+        TestimonyIndex = 0
+    } else {
+        TestimonyIndex += 1
+    }
+}
+
 let TestimonyIndex = 0;
 function fillTestimony(){
     document.getElementById('TestimonySaying').innerHTML =  "<p>" + TestimonyData[TestimonyIndex].Tanggap + "</p>"
@@ -103,20 +144,147 @@ function SetPageIframe(WhichMenu){
     MenuIsOnRightNow = WhichMenu;
     document.getElementById("EmbeddedPage").src = MenuIsOnRightNow;
 }
+function SearchFlight(){
+
+}
+var usernamer;
+var passwordr;
+function ValidateLogin(){
+    usernamer = document.getElementById("Emailo").value
+    passwordr = document.getElementById("Passwordo").value
+    var addAt = 0;
+    var addDot = 0;
+    //check whether there is @
+    for(let i=0; i<usernamer.length; i++){
+        if(usernamer[i] == "@"){
+            addAt++;
+            break;
+        }
+    }
+    if(addAt==0){
+        alert("Invalid Email! '@' missing ")
+        return;
+    }
+
+    for(let i=0; i<usernamer.length; i++){
+        if(usernamer[i] == "."){
+            addDot++;
+            break;
+        }
+    }
+    if(addAt==0){
+        alert("Invalid Email! '.' missing ")
+        return;
+    }
+
+    if(passwordr.length == 0){
+        alert("Password Required!");
+        return;
+    }
+
+    // var num = 0;
+    // var alp = 0;
+
+    // for (let i=0; i<password.length; i++){
+    //     if(password[i] >= '0' && password[i] <= '9'){
+    //         num++;
+    //     }
+    //     if((password[i] >= 'a' && password[i] <= 'z') || (password[i]>='A' && password[i]<='Z')){
+    //         alp++;
+    //     }
+    //     if(alp>0&&num>0){
+    //         break;
+    //     }
+    // }
+    // if(alp == '0' || num == 0){ //=== checks if data type same. === value and datatype
+    //     alert("PASSWORD MUST BE ALPHANUMERIC")
+    //     return;
+    // }
+    var matchEmail = false;
+    var matchPass = false;
+    for (let index = 0; index < LoginData.length; index++) {
+        const element = LoginData[index];
+        if (usernamer == element.email) {
+            matchEmail = true;
+        }
+        if(passwordr == element.pass){
+            matchPass = true;
+        }
+        if(matchPass && matchEmail){
+            break;
+        } else {
+            matchEmail = false;
+            matchPass = false;
+        }
+    }
+
+    if(matchEmail && matchPass){
+        // alert("OK")
+        
+        HaveLoggedIn = true;
+        LoggedInAs = usernamer;
+        //document.getElementById("UseringName").innerHTML = LoggedInAs
+        // document.getElementById("nViewProfile").fadeIn()
+        $('#nViewProfile').css('display','flex')
+        console.log("Logged in: " + LoggedInAs)
+    } else {
+        alert("Invalid Email / Password")
+    }
+}
+function ValidateRegister(){
+    
+}
 
 $("#nHome").click(function(){
+    ClearAddableBackGroundButton();
     SetPageIframe(MenuEnums.Home);
+    AddAddableBackGroundButtonOn(0)
+})
+$("#SubNFlight").click(function(){
+    ClearAddableBackGroundButton();
+    SetPageIframe(MenuEnums.Flight)
+    AddAddableBackGroundButtonOn(1)
+})
+$("#SubNHotel").click(function(){
+    ClearAddableBackGroundButton();
+    SetPageIframe(MenuEnums.Hotel)
+    AddAddableBackGroundButtonOn(1)
+})
+$("#nTestimony").click(function(){
+    ClearAddableBackGroundButton();
+    SetPageIframe(MenuEnums.Testimony);
+    AddAddableBackGroundButtonOn(2)
 })
 $("#nContact").click(function(){
+    ClearAddableBackGroundButton();
     SetPageIframe(MenuEnums.Contact);
+    AddAddableBackGroundButtonOn(3)
 })
 $("#nAccountButton").click(function(){
+    ClearAddableBackGroundButton();
     SetPageIframe(MenuEnums.Registration);
     // if(!HaveLoggedIn){
     //     SetPageIframe(MenuEnums.Registration);
     // } else {
     //     SetPageIframe(MenuEnums.Login);
     // }
+    AddAddableBackGroundButtonOn(4)
+})
+$("#nSignInButton").click(function(){
+    ClearAddableBackGroundButton();
+    SetPageIframe(MenuEnums.Login);
+    AddAddableBackGroundButtonOn(6)
+})
+
+$("#DefTravelFlight").click(function(){
+    ClearAddableBackGroundButton();
+    SetPageIframe(MenuEnums.Flight)
+    AddAddableBackGroundButtonOn(1)
+})
+$("#DefTravelHotel").click(function(){
+    ClearAddableBackGroundButton();
+    SetPageIframe(MenuEnums.Flight)
+    AddAddableBackGroundButtonOn(1)
 })
 
 // $("#nTravels").hover(function(event) {
